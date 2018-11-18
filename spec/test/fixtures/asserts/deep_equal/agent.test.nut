e3172282a8ff9a083cf9d070ac9d037019e494dd
YaroslavYaroslavtsev <Vjqubn[f,79>
2018-11-19 00:33:54 +0700

    assert tests added

---
+++ spec/test/fixtures/asserts/deep_equal/agent.test.nut
@@ -0,0 +1,102 @@
+// MIT License
+//
+// Copyright 2018 Electric Imp
+//
+// SPDX-License-Identifier: MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+class TestCase1 extends ImpTestCase {
+  function test1() {
+    this.assertDeepEqual(
+      /* expected */
+      {
+        "a" : 1,
+        "b" : {
+          "c" : 3
+        }
+      },
+      /* actual */
+      {
+        "a" : 1,
+        "b" : {
+//          "c" : 3
+        }
+      }
+    );
+  }
+
+  function test2() {
+    this.assertDeepEqual(
+      /* expected */
+      {
+        "a" : 1,
+        "b" : {
+          "c" : 3
+        }
+      },
+      /* actual */
+      {
+        "a" : 1,
+        "b" : {
+          "c" : 3,
+          "d" : 4
+        }
+      }
+    );
+  }
+
+  function test3() {
+    this.assertDeepEqual(
+      /* expected */
+      {
+        "a" : 1,
+        "b" : {
+          "c" : 3
+        }
+      },
+      /* actual */
+      {
+        "a" : 1,
+        "b" : {
+          "c" : 100
+        }
+      }
+    );
+  }
+
+function test4() {
+    this.assertDeepEqual(
+      /* expected */
+      {
+        "a" : 1,
+        "b" : {
+          "c" : 3
+        }
+      },
+      /* actual */
+      {
+        "a" : 1,
+        "b" : {
+          "c" : 3
+        }
+      }
+    );
+  }
+}
---
+++ spec/test/test_run_asserts.spec.js
@@ -0,0 +1,154 @@
+// MIT License
+//
+// Copyright 2018 Electric Imp
+//
+// SPDX-License-Identifier: MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+require('jasmine-expect');
+
+const ImptTestHelper = require('../ImptTestHelper');
+const ImptTestCommandsHelper = require('./ImptTestCommandsHelper');
+
+// Deep equal tests
+fdescribe('impt test run for asserts scenario >', () => {
+    beforeAll((done) => {
+        ImptTestHelper.init().
+            then(ImptTestCommandsHelper.cleanUpTestEnvironment).
+            then(ImptTestCommandsHelper.createTestProductAndDG).
+            then(done).
+            catch(error => done.fail(error));
+    }, ImptTestHelper.TIMEOUT);
+
+    afterAll((done) => {
+        ImptTestCommandsHelper.cleanUpTestEnvironment().
+            then(() => ImptTestHelper.cleanUp).
+            then(done).
+            catch(error => done.fail(error));
+    }, ImptTestHelper.TIMEOUT);
+
+    it('test assert true', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/true', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Failed to assert that condition is true/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+
+    it('test assert equal', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/equal', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Expected value: true, got: false/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+
+    it('test assert greater', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/greater', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Failed to assert that 2 > 2/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+
+    it('test assert less', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/less', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Failed to assert that 2 < 2/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+
+    it('test assert close', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/close', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Expected value: 9±0.5, got: 10/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+
+    it('test assert deep equal', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/deep_equal', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Missing slot \[a\.b\.c\] in actual value/);
+                expect(commandOut.output).toMatch(/Extra slot \[a\.b\.d\] in actual value/);
+                expect(commandOut.output).toMatch(/At \[a\.b\.c\]: expected \"3\", got \"100\"/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+
+    it('test assert between', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/between', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Expected value the range of 11..12, got 10/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+
+    it('test assert throw', (done) => {
+        ImptTestCommandsHelper.createTestConfig('fixtures/asserts/throw', {}).
+            then(() => ImptTestHelper.runCommand('impt test run', (commandOut) => {
+                expect(commandOut.output).not.toBeEmptyString();
+                expect(commandOut.output).toMatch(/Success/);
+                expect(commandOut.output).toMatch(/Function was expected to throw an error/);
+                ImptTestCommandsHelper.checkTestFailStatus(commandOut);
+                ImptTestHelper.checkFailStatus(commandOut);
+            })).
+            then(done).
+            catch(error => done.fail(error));
+    });
+});
